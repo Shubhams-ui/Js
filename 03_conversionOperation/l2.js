6c31b5db07b53b2a8d6eb5ab5eb79e2c5d12f7f9
Shubhams-ui <shubhams22036@gmail.com>
2026-03-21 20:56:15 +0530

    first commit

---
+++ 03_conversionOperation/l2.js
@@ -0,0 +1,28 @@
+// ************************* Operations *************************
+
+let value =3
+let negvalue=-value //the value will be turned into its negative
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**4);//it is power after **
+console.log(2/3);
+console.log(2%3);// remainder
+
+let str1="Hello"
+let str2=" Shubham"
+
+let str3= str1+str2 // merges the two strings
+console.log(str3);
+
+"problem occurs here"
+console.log("1"+2);// we get 12
+console.log(1+"2");// we get 12
+console.log("1"+2+2);// we get 122
+console.log(1+2+"2");// we get 32
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
