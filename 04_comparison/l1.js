cf0c45b50a4c4c96cd9a05a5e82ac656b9d8e5c2
Shubhams-ui <shubhams22036@gmail.com>
2026-03-22 23:44:16 +0530

    first commit

---
+++ 04_comparison/l1.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2>=1);
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);// Comparison operator converts null first into number
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);// undefined will give always false
+
+console.log("2"==2);// This will give true
+console.log("2"===2);// This is strict check means it will give as both are different data types
