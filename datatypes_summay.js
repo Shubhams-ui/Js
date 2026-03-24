1a7416d1162a34a9eb1c18efa0102041a8e8eccd
Shubhams-ui <shubhams22036@gmail.com>
2026-03-24 22:05:32 +0530

    first commit

--- datatypes_summay.js
+++ datatypes_summay.js
@@ -23,7 +23,28 @@ let myobj={
 const myfunction=function(){
     console.log("Hello world");
 }
-//Checking types
+//Checking types  
 console.log(typeof(myfunction));
 console.log(typeof(heros));
 console.log(typeof(id));
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stck (Primitive), Heap (Non-Primitive)
+let myName="shubham"
+
+let anothername= myName
+console.log(anothername);
+anothername="Abcd"
+console.log(myName);
+console.log(anothername);
+
+let user={
+    email: "shub@gmail.com",
+    upi: "sh@ybl"
+}
+// Here both gets same reference
+let user2=user// So  value changes in both
+
+user2.email="abs@gmail.com"
+console.log(user.email);
